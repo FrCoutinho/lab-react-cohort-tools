ed01baec65119712f2c3af6918fc6400c5e4eeda
Francisco Coutinho <francisco.coutinho.so@gmail.com>
2024-04-03 19:04:31 +0100

    PT_FT_FranciscoCoutinho

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
 import "./App.css";
+import { Switch } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
 
-
-function App() {
-
+const App = () => {
   return (
-    <div className="App relative z-20 pt-20">
-      <Navbar />
-
-      <div className="pages">
-        <HomePage />
-
-        <StudentDetailsPage />
-
-        <UserProfilePage />
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Routes path="/" component={HomePage} />
+          <Routes path="/students/:studentId" component={StudentDetailsPage} />
+          <Routes path="/profile" component={UserProfilePage} />
+          <Routes path="*" component={HomePage} />
+        </Switch>
       </div>
-
-    </div>
+    </Router>
   );
-}
+};
 
 export default App;
